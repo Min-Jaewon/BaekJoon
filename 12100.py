b95028678f7aa7ab12cce5872e014d7823606ee2
Min-Jaewon <mjw0321@korea.ac.kr>
2022-07-05 20:32:58 +0900

    move 수정

--- 12100.py
+++ 12100.py
@@ -21,20 +21,22 @@ class Move:
     
     def Up(board):
         moved_board=[[0]*n for _ in range(n)]
-        for i in range(n):
-            moved_board[i]=board[i][1:]
-            moved_board[i].insert(n-1, 0)
-            moved_board[i][0]=moved_board[i][0]+board[i][0]
+        for j in range(n):
+            for i in range(n-1):
+                moved_board[i][j]=board[i+1][j]
+            moved_board[n-1][j]=0
+            moved_board[0][j]=moved_board[0][j]+board[0][j]
         return moved_board
     
     def Down(board):
-        moved_board=[0 for _ in range(n)]
-        for i in range(n):
-            moved_board[i]=board[i][1:]
-            moved_board[i].insert(n-1, 0)
-            moved_board[i][0]=moved_board[i][0]+board[i][0]
+        moved_board=[[0]*n for _ in range(n)]
+        for j in range(n):
+            for i in range(1, n):
+                moved_board[i][j]=board[i-1][j]
+            moved_board[0][j]=0
+            moved_board[n-1][j]=moved_board[n-1][j]+board[n-1][j]
         return moved_board
     
-moved=Move.Right(board)
+moved=Move.Down(board)
 for i in range(n):
     print(moved[i])
